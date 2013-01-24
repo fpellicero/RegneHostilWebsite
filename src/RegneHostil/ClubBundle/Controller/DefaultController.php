ade46be259d6460e631c087f4a8df7857170efc2
Francesc Pellicero <pelly.obn91@gmail.com>
2013-01-24 13:16:50 +0100

    Esborrat Acme bundle

--- src/RegneHostil/ClubBundle/Controller/DefaultController.php
+++ src/RegneHostil/ClubBundle/Controller/DefaultController.php
@@ -3,11 +3,131 @@
 namespace RegneHostil\ClubBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+use RegneHostil\ClubBundle\Entity\Noticia;
+use RegneHostil\ClubBundle\Entity\Quote;
 
 class DefaultController extends Controller
 {
-    public function indexAction($name)
-    {
-        return $this->render('RegneHostilClubBundle:Default:index.html.twig', array('name' => $name));
+	/**
+	 * Controller method for the homepage
+	 *
+	 */
+    public function indexAction()
+	{
+		// We set the offset based on the page parameter
+		$page = 0;
+		if(isset($_GET['page'])) {
+			$page = $_GET['page'];
+		}
+		$offset = $page * 5;
+
+		// Building the query to fetch the objects from db
+		$repository = $this->getDoctrine()
+			->getRepository('RegneHostilClubBundle:Noticia');
+		$query = $repository->createQueryBuilder('n')
+			->setFirstResult( $offset )
+			->setMaxResults ( 5 )
+			->getQuery();
+
+		// Fetch the objects
+		$noticies = $query->getResult();
+
+		// Decide wether to put the 'older' and 'newer' button or not
+		$older = true;
+		if(sizeof($noticies) < 5) {
+			$older = false;	
+		}
+		$newer = true;
+		if($offset == 0) {
+			$newer = false;
+		}
+
+		return $this->render(
+			'RegneHostilClubBundle:Default:index.html.twig',
+			array(
+				'noticies' => $noticies,
+				'numpage' => $page,
+				'newer' => $newer,
+				'older' => $older
+			)
+		);
     }
+
+	/**
+	 * Controller method for the about_us page
+	 *
+	 */
+	public function aboutAction()
+	{
+			return $this->render('RegneHostilClubBundle:Default:about.html.twig');
+	}
+
+	/**
+	* Controller method for the contact page
+	*
+	*/
+	public function contactAction()
+	{
+		$form = $this->createFormBuilder()
+			->add('name','text')
+			->add('email','email')
+			->add('text','textarea')
+			->getForm();
+
+		/* Generate the captcha script to embed */
+		require_once('bundles/regnehostilclub/recaptchalib.php');
+		$publickey = "6LeVw8cSAAAAAC1mXZ_IQI-UCUl2XNGy6g3Ex0St";
+		$captcha = recaptcha_get_html($publickey);
+
+		return $this->render(
+			'RegneHostilClubBundle:Default:contact.html.twig',
+			array(
+				'form' => $form->createView(),
+				'captcha' => $captcha
+				)
+			);
+	}
+
+	/**
+	 * Controller method to create Noticies
+	 *
+	 */
+	public function createAction(Request $request)
+	{
+		$noticia = new Noticia();
+
+		$form = $this->createFormBuilder($noticia)
+			->add('title','text')
+			->add('date','text')
+			->add('text','textarea')
+			->getForm();
+
+		if ($request->isMethod('POST')) {
+			$form->bind($request);
+
+			if($form->isValid()) {
+				$em = $this->getDoctrine()->getManager();
+				$em->persist($noticia);
+				$em->flush();
+
+				return $this->render(
+					'RegneHostilClubBundle:Admin:create.html.twig',
+					array(
+						'form' => $form->createView(),
+						'created' => true
+					)
+				);
+			}
+		}
+		return $this->render(
+			'RegneHostilClubBundle:Admin:create.html.twig',
+			array(
+					'form' => $form->createView(),
+					'created' => false
+			)
+		);
+	}
 }
---
+++ src/RegneHostil/ClubBundle/Entity/Noticia.php
@@ -0,0 +1,124 @@
+<?php
+
+namespace RegneHostil\ClubBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Noticia
+ *
+ * @ORM\Table(name="noticia")
+ * @ORM\Entity
+ */
+class Noticia
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="title", type="string", length=100)
+     */
+    private $title;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="date", type="string", length=20)
+     */
+    private $date;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="text", type="text")
+     */
+    private $text;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set title
+     *
+     * @param string $title
+     * @return Noticia
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+    
+        return $this;
+    }
+
+    /**
+     * Get title
+     *
+     * @return string 
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * Set date
+     *
+     * @param string $date
+     * @return Noticia
+     */
+    public function setDate($date)
+    {
+        $this->date = $date;
+    
+        return $this;
+    }
+
+    /**
+     * Get date
+     *
+     * @return string 
+     */
+    public function getDate()
+    {
+        return $this->date;
+    }
+
+    /**
+     * Set text
+     *
+     * @param string $text
+     * @return Noticia
+     */
+    public function setText($text)
+    {
+        $this->text = $text;
+    
+        return $this;
+    }
+
+    /**
+     * Get text
+     *
+     * @return string 
+     */
+    public function getText()
+    {
+        return $this->text;
+    }
+}
